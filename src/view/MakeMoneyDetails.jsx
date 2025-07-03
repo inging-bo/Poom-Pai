37cb20cc9a424b668c6219a209b0b6f133a85a43
inging-bo <thervv@kakao.com>
2025-07-03 22:34:43 +0900

    UI 퍼블리싱

--- src/view/MakeMoneyDetails.jsx
+++ src/view/MakeMoneyDetails.jsx
@@ -5,10 +5,25 @@ function makeMoneyDetails({ setOpenModal }) {
   
   return (
     <div className="flex flex-col p-5 w-full gap-4">
-      <h2 className="text-main-text text-3xl">모임 제목</h2>
-      <input className="placeholder:text-sub-color placeholder:font-money border-[6px] h-14 text-xl px-2 border-main-color rounded-lg" type="text" id="pass" name="password" minLength="8" placeholder="모임 제목을 입력하세요" required/>
+      {/* 모임 제목 */}
+      <div className="flex flex-col gap-2">
+        <h2 className="text-main-text text-3xl">모임 제목</h2>
+        <input
+          className="h-14 text-xl text-main-text placeholder:text-sub-color placeholder:text-lg placeholder:font-money border-[6px] px-2 border-main-color rounded-lg"
+          type="text" id="" name="name" minLength="8" placeholder="모임 제목을 입력하세요" required/>
+      </div>
+      {/* 입장 코드 */}
+      <div className="flex items-center gap-2">
+        <h2 className="text-main-text text-2xl">입장 코드</h2>
+        <input
+          className="flex-1 w-full text-main-text placeholder:text-sub-color placeholder:font-money border-[6px] h-14 px-2 border-main-color rounded-lg"
+          inputMode="numeric" pattern="[0-9]*" minLength="8" placeholder="코드 입력" required/>
+      </div>
+      {/* 버튼 */}
       <div className="text-main-text flex justify-between">
-        <button className="px-1 py-2 w-32 text-2xl border-[6px] border-main-color rounded-lg" onClick={() => goHome()}>등록 취소</button>
+        <button className="px-1 py-2 w-32 text-2xl border-[6px] border-main-color rounded-lg"
+                onClick={() => goHome()}>등록 취소
+        </button>
         <button className="px-1 py-2 w-32 text-2xl bg-main-color text-white rounded-lg">등록</button>
       </div>
     </div>
--- src/view/MoneyDetails.jsx
+++ src/view/MoneyDetails.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { motion as Motion} from "framer-motion";
+import { motion as Motion } from "framer-motion";
 
 function MoneyDetails() {
   
@@ -11,95 +11,80 @@ function MoneyDetails() {
   
   return (
     <Motion.div
-      className="relative min-h-svh w-screen max-w-xl my-0 mx-auto flex flex-col justify-center items-center"
+      className="relative min-h-svh w-screen max-w-xl my-0 mx-auto flex flex-col justify-start items-center"
       initial={{ x: "100%" }}
       animate={{ x: 0 }}
       exit={{ x: "100%" }}
       transition={{ duration: 0.4 }}
     >
-      <div className="flex bg-[#000000] text-white w-full sticky top-0 justify-between">
-        <div>
-          <div>
+      <div
+        className="flex bg-main-bg border-b-2 border-main-color items-center w-full sticky top-0 justify-between pt-2 pb-1 px-2">
+        {/* 상단 */}
+        <div className="flex flex-col justify-center">
+          <div className="text-2xl text-center">
             총 경비
           </div>
-          <div>
+          <div className="bg-main-bg text-main-text px-2 py-1 text-xl font-money">
             1,000,000원
           </div>
         </div>
         <div>
-          <div>
+          <div className="text-2xl text-center">
             남은 금액
           </div>
-          <div>
+          <div className="bg-main-bg text-main-text px-2 py-1 text-xl font-money">
             1,000,000원
           </div>
         </div>
       </div>
-      <table className="border-2 border-black border-collapse">
-        <thead>
-        <tr>
-          <th scope="col">참여자</th>
-          <th scope="col">내야할 돈</th>
-          <th scope="col">지불 금액</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td>집</td>
-          <td>HTML tables</td>
-          <td>22</td>
-        </tr>
-        <tr>
-          <td>어디</td>
-          <td>Web accessibility</td>
-          <td>45</td>
-        </tr>
-        <tr>
-          <td>고기</td>
-          <td>JavaScript frameworks</td>
-          <td>29</td>
-        </tr>
-        <tr>
-          <td>하이</td>
-          <td>Web performance</td>
-          <td>36</td>
-        </tr>
-        </tbody>
-      </table>
-      <table className="border-2 h-[1500px] border-black border-collapse">
-        <thead>
-        <tr>
-          <th scope="col">사용처</th>
-          <th scope="col">사용금액</th>
-          <th scope="col">제외될 사람</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td>집</td>
-          <td>HTML tables</td>
-          <td>22</td>
-        </tr>
-        <tr>
-          <td>어디</td>
-          <td>Web accessibility</td>
-          <td>45</td>
-        </tr>
-        <tr>
-          <td>고기</td>
-          <td>JavaScript frameworks</td>
-          <td>29</td>
-        </tr>
-        <tr>
-          <td>하이</td>
-          <td>Web performance</td>
-          <td>36</td>
-        </tr>
-        </tbody>
-      </table>
-      <div className="fixed max-w-xl bottom-10 flex w-full justify-between">
-        <div onClick={() => goHome()} className="border-2 border-black">뒤로가기</div>
-        <div className="border-2 border-black">인원추가</div>
+      {/* 참여자 */}
+      <ul className="flex flex-col gap-2 w-full py-4 px-2">
+        <li className="flex">
+          <span className="basis-[32%] text-2xl text-center">참석자</span>
+          <span className="basis-[38%] text-2xl text-center">뿜빠이</span>
+          <span className="basis-[38%] text-2xl text-center">지불 금액</span>
+        </li>
+        <li className="flex text-xl font-money">
+          <span className="basis-[32%] text-center">이름</span>
+          <span className="basis-[38%] text-right">10,000<span>원</span></span>
+          <span className="basis-[38%] text-right">100,000<span>원</span></span>
+        </li>
+        <li className="flex text-xl font-money">
+          <span className="basis-[32%] text-center">이름</span>
+          <span className="basis-[38%] text-right">10,000<span>원</span></span>
+          <span className="basis-[38%] text-right">100,000<span>원</span></span>
+        </li>
+        <li className="p-3 text-center text-xl text-main-color border-2 border-sub-color mx-[10%] rounded-full">인원 추가
+          +
+        </li>
+      </ul>
+      {/* 사용처 */}
+      <ul className="flex flex-col gap-2 w-full py-4 px-2 mb-10">
+        <li className="flex">
+          <span className="flex-1 text-2xl text-center">사용처</span>
+          <span className="flex-1 text-2xl text-center">사용 금액</span>
+          <span className="flex-1 text-2xl text-center">제외 인원</span>
+        </li>
+        <li className="flex text-xl font-money flex-wrap">
+          <span className="flex-1 text-center">이름</span>
+          <span className="flex-1 text-right">10,000<span>원</span></span>
+          <span className="flex-1 text-right">100,000<span>원</span></span>
+        </li>
+        <li className="flex text-xl font-money">
+          <span className="flex-1 text-center">이름</span>
+          <span className="flex-1 text-right">10,000<span>원</span></span>
+          <span className="flex-1 text-right">100,000<span>원</span></span>
+        </li>
+        <li className="p-3 text-center text-xl text-main-color border-2 border-sub-color mx-[10%] rounded-full">인원 추가
+          +
+        </li>
+      </ul>
+      <div className="fixed flex justify-around max-w-xl bottom-10 w-full">
+        <button onClick={() => goHome()}
+             className="px-1 py-2 w-32 text-2xl border-[6px] bg-main-bg border-main-color rounded-lg">
+          뒤로가기
+        </button>
+        <button className="px-1 py-2 w-32 text-2xl bg-main-color text-white rounded-lg">저장</button>
       </div>
     </Motion.div>
   )

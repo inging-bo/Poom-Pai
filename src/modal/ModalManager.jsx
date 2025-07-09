0a4ba8e254b739a8d5bc9c7c5677760f8e87c77c
inging-bo <thervv@kakao.com>
2025-07-09 23:13:04 +0900

    Update ModalManager.jsx

--- src/modal/ModalManager.jsx
+++ src/modal/ModalManager.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import ModalNotice from './ModalNotice.jsx';
 import ModalEditMode from './ModalEditMode.jsx';
 
 import { useModalStore } from '../store/modalStore.js';
 
 // 모달 타입과 컴포넌트 매핑
 const MODAL_COMPONENTS = {
-  ModalNotice,       // ModalNotice 컴포넌트
   ModalEditMode,
   none: () => null,  // 모달이 없는 경우 (null 반환)
 };
